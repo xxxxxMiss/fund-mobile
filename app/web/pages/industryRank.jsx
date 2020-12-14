641fe02967e3482a5ef9b63f7c6295acb86b374c
chenxianlong <794465731@qq.com>
2020-12-15 00:10:51 +0800

    Add header for industry

--- app/web/pages/industryRank.jsx
+++ app/web/pages/industryRank.jsx
@@ -2,7 +2,6 @@ import { get } from 'utils/request'
 import { fmtRate, fmtNumber } from 'utils/format'
 import BScroll from '@better-scroll/core'
 import { useEffect, useRef } from 'react'
-
 export default function IndustryRank(props) {
   const scrollerRef = useRef(null)
   const elRef = useRef(null)
@@ -19,6 +18,7 @@ export default function IndustryRank(props) {
   return (
     <div className={sbx('page-industry')}>
       <div className={sbx('fixed-panel')}>
+        <div className={sbx('list-item')}>板块名称</div>
         {(props.fixedList || []).map(item => {
           return (
             <div className={sbx('list-item')} key={item.code}>
@@ -30,6 +30,12 @@ export default function IndustryRank(props) {
       </div>
       <div className={sbx('scroll-panel')} ref={elRef}>
         <div className={sbx('scroller-wrapper')}>
+          <div className={sbx('list-item')}>
+            <div className={sbx('item')}>最新价格</div>
+            <div className={sbx('item')}>涨跌额</div>
+            <div className={sbx('item')}>涨跌幅度</div>
+            <div className={sbx('item')}>成交量</div>
+          </div>
           {(props.list || []).map(item => {
             const { text, color } = fmtRate(item.changePercent)
             const averagePrice = fmtNumber(item.averagePrice)
