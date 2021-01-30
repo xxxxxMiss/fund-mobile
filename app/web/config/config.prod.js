0d06276e02e09278dd811cc92e07beaa1f4bf725
chenxianlong <794465731@qq.com>
2021-01-30 16:36:21 +0800

    Add sentry with umi-plugin-sentry

--- app/web/config/config.prod.js
+++ app/web/config/config.prod.js
@@ -1,4 +1,12 @@
+import { join } from 'path'
+
 export default {
+  plugins: [join(__dirname, '..', 'plugins', 'umi-plugin-sentry.js')],
+  sentry: {
+    dsn:
+      'https://e72ed2d5dff94233988280d94decf530@o512888.ingest.sentry.io/5615608',
+    tracesSampleRate: 1.0,
+  },
   define: {
     'process.env.axiosBaseURL': '',
     'process.env.ssrAxiosBaseURL': 'http://127.0.0.1:7001',
---
+++ app/web/plugins/umi-plugin-sentry.js
@@ -0,0 +1,28 @@
+export default api => {
+  api.addEntryCodeAhead(() => {
+    return `
+      import * as Sentry from '@sentry/react'
+      import { Integrations } from '@sentry/tracing'
+
+      Sentry.init({
+        dsn: '${api.config.sentry.dsn}',
+        integrations: [new Integrations.BrowserTracing()],
+
+        // We recommend adjusting this value in production, or using tracesSampler
+        // for finer control
+        tracesSampleRate: ${api.config.sentry.tracesSampleRate}
+      })
+    `
+  })
+  api.describe({
+    key: 'sentry',
+    config: {
+      schema(joi) {
+        return joi.object({
+          dsn: joi.string(),
+          tracesSampleRate: joi.number().min(0).max(1.0),
+        })
+      },
+    },
+  })
+}
